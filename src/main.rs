4cba34c00b159185c4f9e39781ea1722f719ea13
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-08-31 15:44:04 +0100

    3 pages

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Yo waddup" }));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/about", get(about))
+        .route("/blog", get(blog).post(blog2));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+async fn root() -> String { "Yo waddup".to_string() }
+async fn about() -> String { "I'm super cool".to_string() }
+async fn blog() -> String { "Will I write a blog?\n".to_string() }
+async fn blog2() -> String { "Yeah maybe someday".to_string() }
