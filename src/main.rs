68bcaf58a48e309292a3c30a828c9321218a2e2e
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-08-31 15:38:10 +0100

    deployed

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use axum::{routing::get, Router};
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Yo waddup" }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener,app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
