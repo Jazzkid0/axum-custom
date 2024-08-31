a1218e1918666595bea82dfaded30e480cfdbce6
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-08-31 16:07:49 +0100

    its giving HTML

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::Html};
 
 #[tokio::main]
 async fn main() {
@@ -11,7 +11,48 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> String { "Yo waddup".to_string() }
-async fn about() -> String { "I'm super cool".to_string() }
-async fn blog() -> String { "Will I write a blog?\n".to_string() }
+async fn root() -> Html<String> { 
+    Html(r#"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <title>homepage</title>
+</head>
+<body>
+    <h1>Lfg</h1>
+    <p>Yo waddup</p>
+
+    <ul>
+        <li><a href="/about">about</a></li>
+        <li><a href="/blog">blog</a></li>
+    </ul>
+</body>
+"#.to_string())
+}
+
+async fn about() -> Html<String> { Html("I'm super cool".to_string()) }
+
+async fn blog() -> Html<String> { 
+    Html(r#"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <title>rust blog</title>
+</head>
+<body>
+    <h1>Blog woo</h1>
+    <p>Idk if html will serve properly like this</p>
+    <h2>let's try a list</h2>
+    <ul>
+        <li>Post number 1</li>
+        <li>Post number 2</li>
+    </ul>
+</body>
+</html>
+"#.to_string())
+}
 async fn blog2() -> String { "Yeah maybe someday".to_string() }
